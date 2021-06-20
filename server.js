026bcd2f77d83bfa489ecc551cf64b663811ac4b
lmclaramunt <lclaramu@asu.edu>
2021-06-19 20:56:15 -0700

    Listening when battery changes

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
+const { response } = require('express');
 const express = require('express');
 const app = express();
 const path = require('path');
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const port = process.env.PORT || 3000;
-const { addUser, getUser, getAllUsers } = require('./users');
+const { addUser, getAllUsers,
+  newAdmin, updateAdmin, getUsername, updateBattery
+   } = require('./users');
 
 server.listen(port, () => {
   console.log(`Server listening at port ${port}`);
@@ -16,13 +19,67 @@ app.use(express.static(path.join(__dirname, 'public')));
 io.on('connection', (socket) => {
   
   socket.on('login', (data) =>{
-    const user = addUser(socket.id, data.username, data.battery, data.latitude, data.longitude);
-    socket.broadcast.emit('userAdded', (data));
+    addUser(socket.id, data.username, data.battery, 
+      data.latitude, data.longitude, (err, response) => {
+        if(err){
+          socket.emit('login results', {results: false});   // User not able to login
+        }else if(!response){ 
+          // User won't be admin by defaul when added to the Map, unless is the first one
+          // So let's check if he/she should be the new admin 
+          newAdmin(socket.id, data.battery, (response) => {  
+            if(response){   
+              // It's been confirmed that the new user should be the new admin, so update the Map and user's
+              updateAdmin(socket.id, () => {
+                 // Let other know about new user, who is the admin
+                socket.broadcast.emit('userAdded', ({ ...data, admin: true}));        
+              });              
+            }else{
+              // The admin has not changed
+              socket.broadcast.emit('userAdded', ({...data, admin: false}));
+            }
+          });
+          socket.emit('login results', {results: true});
+        }else{
+          // If it's the first user to login, then it will definetely be assigned the admin role
+          socket.broadcast.emit('userAdded', ({...data, admin: true}));
+          socket.emit('login results', {results: true});
+        }
+      });
   });
 
   socket.on('joinLobby', (data) => {
-    console.log('Someone is asking for all users');
-    socket.emit('lobbyUsers', getAllUsers());
+    getAllUsers((users) => {
+      socket.emit('lobbyUsers', users);
+    });
+  });
+
+  socket.on('battery change', (battery) => {
+    getUsername(socket.id, (err, response) => {
+      if(!err){
+        const username = response;
+        updateBattery(socket.id, battery, () => {
+          var userVal = {
+            username: username,
+            battery: battery
+          };
+          newAdmin(socket.id, battery, (response) => {
+            if(response){   
+              // It's been confirmed that there should be a new admin, update the map
+              updateAdmin(socket.id, () => {
+                 // Let other sers know user whose batter changed is now the admin
+                 socket.emit('update user', {...userVal, admin: true});
+                 socket.broadcast.emit('update user', {...userVal, admin: true});
+              });              
+            }else{
+              // Update user values, battert is the only one that has changed
+              socket.emit('update user', {...userVal, admin: false});
+              socket.broadcast.emit('update user', {...userVal, admin: false});
+            }
+          });
+        });
+      }
+    });
+    
   });
 
 });
--- users.js
+++ users.js
@@ -1,24 +1,104 @@
-const usersArray = [];
+const { response } = require("express");
+
+const usersMap = new Map();
 
 // Function used to keep track of users who have joined the server
-function addUser(id, username, battery, latitude, longitude){
-    const user = {id, username, battery, latitude, longitude};
-    usersArray.push(user);
-    return user;
+function addUser(id, username, battery, latitude, longitude, callback){
+    newName(username, (response) => {
+       if(!response){
+        return callback(true, false);
+       }else{
+        const admin = (usersMap.size === 0) ? true : false; 
+        const user = {
+            username: username,
+            battery: battery,
+            latitude: latitude,
+            longitude: longitude,
+            admin: admin
+        };
+        usersMap.set(id, user);
+        return callback(false, admin);  
+       }
+    });
 }
 
-// Find user in the Array that keeps track of users in the server
-function getUser(id){
-    return usersArray.find(user => user.id === id);
+// Check if a name is new in the Map
+function newName(name, callback){
+    for(let [key, user] of usersMap){
+        if (name === user.username){
+            return callback(false);
+        }
+    }
+    return callback(true);
+}
+
+
+// Find user in the Map 
+function getUsername(id, callback){
+    if(usersMap.has(id)){
+        return callback(false, usersMap.get(id).username);
+    }else{
+        return callback(true, null);
+    }
+}
+
+// Check if there should be a new admin based on battery level
+function newAdmin(id, battery, callback){
+    getAdminBattery((response) => {
+        if(battery > response.battery &&
+            id != response.id){
+            return callback(true);
+        }else{
+            return callback(false);
+        }
+    })
+}
+
+// Get user with max battery, it should be the admin 
+function getAdminBattery(callback){
+    var adminBattery = 0;
+    var adminUser = 0;
+    for(let [key, user] of usersMap){
+        if(user.admin){
+            adminBattery = user.battery;
+            adminUser = key;
+        }
+    }
+    return callback({id: adminUser, battery: adminBattery})
+}
+
+// Update who is the admin
+function updateAdmin(newAdminId, callback){
+    for(let [key, user] of usersMap){
+        user.admin = (key === newAdminId) ? true: false;
+    }
+    return callback();
+}
+
+// Update the battery for a user
+function updateBattery(id, battery, callback){
+    for(let [key, user] of usersMap){
+        if(key === id){
+            user.battery = battery;
+        }
+    }
+    return callback();
 }
 
 // Get all users that are currently in the server
-function getAllUsers(){
-    return usersArray;
+function getAllUsers(callback){
+    const usersArray = [];
+    for(let [key, user] of usersMap){
+        usersArray.push(user);
+    }
+    return callback(usersArray);
 }
 
 module.exports = {
     addUser,
-    getUser,
-    getAllUsers
+    getUsername,
+    getAllUsers,
+    newAdmin,
+    updateAdmin,
+    updateBattery
 }
